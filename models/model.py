d35c41a06045449f2914a84c0213c1eb850f4a36
beginner <yiminl.edu@gmail.com>
2026-02-18 16:19:51 -0800

    First Author

--- models/model.py
+++ models/model.py
@@ -1,5 +1,5 @@
 # CosCNN-DTQ Toolbox
-# Copyright (C) 2024-2025 Guoyang Liu
+# Copyright (C) 2024-2025 Yimin Liu
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- toolbox_cli.py
+++ toolbox_cli.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # CosCNN-DTQ Toolbox
-# Copyright (C) 2024-2025 Guoyang Liu
+# Copyright (C) 2024-2025 Yimin Liu
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
